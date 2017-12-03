fc86d372c0d00b81fc20e9780a1936d1847549a4
Yonathan <yoni.kid15@gmail.com>
2017-12-02 19:15:10 -0500

    Setup data fetching with database

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -24,6 +24,9 @@ export default (state = expensesReducerDefaultState, action) => {
                     return expense;
                 };
             });
+        
+        case 'SET_EXPENSES':
+            return action.expenses;
 
         default: 
             return state;
